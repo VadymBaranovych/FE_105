ac848766d505feaf8c8f6e4e44995db695d98ef1
Vadym Baranovych <126914107+VadymBaranovych@users.noreply.github.com>
2023-05-03 15:32:12 +0300

    Added hw_17

---
+++ hw_17/js/script.js
@@ -0,0 +1,8 @@
+let sidebar = document.getElementsByClassName('sidebar');
+let btnSidebar = document.getElementsByClassName('sidebar_btn');
+let layoutGrid = document.getElementsByClassName('layout');
+
+btnSidebar[0].onclick = function() {
+    layoutGrid[0].classList.toggle('layout__active');
+    sidebar[0].classList.toggle('sidebar__active');
+}
