6e38e5b3e8002faa088e7e9553a91f31558cce87
Vadym Baranovych <126914107+VadymBaranovych@users.noreply.github.com>
2023-05-08 23:32:15 +0300

    fix conflict

--- hw_17/js/script.js
+++
@@ -1,8 +0,0 @@
-let sidebar = document.getElementsByClassName('sidebar');
-let btnSidebar = document.getElementsByClassName('sidebar_btn');
-let layoutGrid = document.getElementsByClassName('layout');
-
-btnSidebar[0].onclick = function() {
-    layoutGrid[0].classList.toggle('layout__active');
-    sidebar[0].classList.toggle('sidebar__active');
-}
