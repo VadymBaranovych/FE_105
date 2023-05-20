7111cfc76cdf73f3e81d7a3d5ad5ee53831e3ac3
Vadym Baranovych <126914107+VadymBaranovych@users.noreply.github.com>
2023-05-20 14:01:00 +0300

    added file

--- exam_2/js/script.js
+++ exam_2/js/script.js
@@ -72,3 +72,25 @@ window.addEventListener("scroll", activeMenu);
 //Fancybox
 Fancybox.bind('[data-fancybox="gallery"]', {
 });
+// Google Map
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("googleMap"), {
+    center: { lat: 40.6686, lng: -73.8999 },
+    zoom: 13.5,
+    mapId: '2f084b4895741534',
+  });
+
+  const marker = new google.maps.Marker({
+    position: { lat: 40.6781, lng: -73.8981 },
+    map: map,
+    title: "Monticello",
+    icon: {
+      url: "https://i.postimg.cc/30jd33nS/Pin.png",
+      scaledSize: new google.maps.Size(100, 100),
+    },
+  });
+}
+
+window.initMap = initMap;
