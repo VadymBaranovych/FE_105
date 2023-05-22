4b7fdf7ef7c2661fe76c9d2c5038ff3c31dcb452
Vadym Baranovych <126914107+VadymBaranovych@users.noreply.github.com>
2023-05-22 09:30:30 +0300

    added file

--- exam_2/js/script.js
+++ exam_2/js/script.js
@@ -58,17 +58,22 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
   });
 });
 //activeMenu 
-const li=document.querySelectorAll(".links");
-const sec=document.querySelectorAll("section");
-
-function activeMenu(){
-  let len=sec.length;
-  while(--len && window.scrollY + 97 < sec[len].offsetTop){}
-  li.forEach(ltx => ltx.classList.remove("active"));
-  li[len].classList.add("active");
-}
-activeMenu();
-window.addEventListener("scroll", activeMenu);
+let section = document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop-150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    if(top > offset && top < offset + height){
+      navLink.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+};
 //Fancybox
 Fancybox.bind('[data-fancybox="gallery"]', {
 });
