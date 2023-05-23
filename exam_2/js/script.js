c52a46f40470c5fea19c46bcdd972fe3336b2ce2
Vadym Baranovych <126914107+VadymBaranovych@users.noreply.github.com>
2023-05-23 21:24:53 +0300

    added file

--- exam_2/js/script.js
+++ exam_2/js/script.js
@@ -69,7 +69,7 @@ window.onscroll = () => {
     if(top > offset && top < offset + height){
       navLink.forEach(links => {
         links.classList.remove('active');
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        document.querySelector('header nav a[href*=" + id + "]').classList.add('active');
       });
     };
   });
